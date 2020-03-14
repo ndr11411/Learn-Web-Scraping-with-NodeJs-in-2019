2b4e4bf6a3f3c5faee3e40fd1ef31402dfdca930
ndr11411 <ndr11411@gmail.com>
2020-03-14 23:41:25 +0100

    Titulo y rating

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const request = require('request-promise')
-const cheerio = require('cheerio')
+const request = require('request-promise');
+const cheerio = require('cheerio');
 
-const URL = 'https://www.imdb.com/title/tt4154796/?ref_=nv_sr_srsg_0'
+const URL = 'https://www.imdb.com/title/tt4154796/?ref_=nv_sr_srsg_0';
 (async () => {
-  const response = await request(URL)
-  console.log(response)
+  const response = await request(URL);
+ // console.log(response)
+ let $ = cheerio.load(response);
+ // queremos el div con la class title_wrape y selecionamos el h1 y lo queremos en txt
+ let title = $('div[class="title_wrapper"] > h1').text();
+ let rating = $('span[itemprop="ratingValue"]').text();
+ let director = $('div[class="credit_summary_item"] > a').text();
+ console.log(title, rating, director);
+
 })()
