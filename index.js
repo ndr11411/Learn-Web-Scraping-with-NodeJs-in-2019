b2b17e603d5270776e1c2b4d717b525ad6b23168
nacho <ndr11411@gmail.com>
2020-03-18 19:56:49 +0100

    con IF

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const request = require('request-promise');
 
 (async () => {
   console.log('Iniciando request')
-  let status = await request('https://httpbin.org/status/200')
-  debugger // hay que escribit estus en la consola de depuracion
+  try {
+    let status = await request({
+      uri: 'https://httpbin.org/status/300',
+      resolveWithFullResponse: true
+    })
+  } catch (response) {
+    if(response.statusCode == 300){
+      console.log("todo esta OK")
+    }else {
+      console.log(`Algo funciono mal: ${response}`)
+      process.exit(1)
+    }
+    // debugger // hay que escribit estus en la consola de depuracion
+  }
+  // debugger
 })()
